c86ffb5878fa20217be2f782f66eb4da3b706da6
Sebastian Hallén <sebastian.hallen@gmail.com>
2013-12-14 21:33:22 +0100

    adding tests for traversability checkers

---
+++ src/Chayka.Tests/Lookup/EdgeCheckingTraversableEdgeCheckerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Chayka.Tests.Lookup
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Chayka.Lookup;
+    using FakeItEasy;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EdgeCheckingTraversableEdgeCheckerTests
+    {
+        [Test]
+        public void Should_query_edge_for_traversability()
+        {
+            var edge = A.Fake<IEdge<IVertex<int>>>();
+            var edgeChecker = new EdgeCheckingTraverseableEdgeChecker<int>();
+
+            edgeChecker.IsTraverseable(edge);
+
+            A.CallTo(() => edge.IsWalkable()).MustHaveHappened();
+        }
+    }
+}
---
+++ src/Chayka.Tests/Lookup/ExternalTraverseableEdgeCheckerTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Chayka.Tests.Lookup
+{
+    using Chayka.Lookup;
+    using FakeItEasy;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExternalTraverseableEdgeCheckerTests
+    {
+        [Test]
+        public void Should_should_call_external_traversability_function_when_checking_if_edge_is_traverseable()
+        {
+            var isTraversableChecked = false;
+            var edgeChecker = new ExternalTraverseableEdgeChecker<int>(_ =>
+                {
+                    isTraversableChecked = true;
+                    return false;
+                });
+
+            edgeChecker.IsTraverseable(A.Fake<IEdge<IVertex<int>>>());
+
+            Assert.That(isTraversableChecked);
+        }
+    }
+}
